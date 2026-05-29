50ffff9271ff4b8c8a531a49d53fb6a815ff9c43
tyctw <tyctw.analyze@gmail.com>
2026-05-30 00:42:45 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,8 @@ export const CONFIG = {
   // API endpoint for sending invitation code
   apiEndpoint: 'https://script.google.com/macros/s/AKfycbwj7dCVukOvJ8MR_PsfkymKDfivZppo0RFkqxpJSMVaYZLiNSRgqMwwOIMXNQZByqdfag/exec',
   
-  // Cooldown time in seconds (8 hours)
-  cooldownTime: 28800,
+  // Cooldown time in seconds (3 hours)
+  cooldownTime: 10800,
   
   // hCaptcha site key
   hcaptchaSiteKey: 'bd02abec-4302-4234-a464-695c3972bccc',
@@ -11,3 +11,4 @@ export const CONFIG = {
   // Site information
   siteName: '桃聯區會考落點分析系統',
 };
+
